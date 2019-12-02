4a0017fc390d85c3ca0e7cad8d1f364a6e13b09f
aleksaboskov <aleksa.boskov@lotusflare.com>
2019-12-02 23:58:51 +0100

    Add NetworkFailure class

---
+++ commons/src/main/java/com/thedeekay/commons/NetworkFailure.kt
@@ -0,0 +1,21 @@
+package com.thedeekay.commons
+
+/**
+ * Sealed class containing failure reason of a network request.
+ *
+ * Subtypes of this can be either generic network errors, embodied by [Generic], or errors specific
+ * to some given request, embodied by [Specific].
+ *
+ * @param E Type of specific error that this can contain.
+ */
+sealed class NetworkFailure<out E> {
+
+    sealed class Generic : NetworkFailure<Nothing>() {
+        object NoInternet : Generic()
+        object Timeout : Generic()
+    }
+
+    data class Specific<out E>(val failure: E) : NetworkFailure<E>()
+}
+
+typealias PureNetworkFailure = NetworkFailure<Nothing>
