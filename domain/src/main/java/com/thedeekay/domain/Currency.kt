4b85fae4ed63ef46db0899622caea73268b97e5b
aleksaboskov <aleksa.boskov@lotusflare.com>
2019-12-02 21:27:14 +0100

    Create basic outline of a Currency

---
+++ domain/src/main/java/com/thedeekay/domain/Currency.kt
@@ -0,0 +1,12 @@
+package com.thedeekay.domain
+
+/**
+ * Domain representation of a real-world currency.
+ */
+data class Currency(
+
+    /**
+     * ISO 4217 code of the currency.
+     */
+    val code: String
+)
