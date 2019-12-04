643720b3b7ba4a64edcc05424ebc7b30ba907375
aleksaboskov <aleksa.boskov@lotusflare.com>
2019-12-04 23:52:14 +0100

    Pull RequestSpecDispatcher to a separate file

--- exchangerates/src/test/java/com/thedeekay/exchangerates/ExchangeRatesNetworkRequestTest.kt
+++ exchangerates/src/test/java/com/thedeekay/exchangerates/ExchangeRatesNetworkRequestTest.kt
@@ -4,12 +4,8 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.thedeekay.commons.Outcome.Success
 import com.thedeekay.domain.*
-import okhttp3.Headers
 import okhttp3.HttpUrl
-import okhttp3.mockwebserver.Dispatcher
-import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
-import okhttp3.mockwebserver.RecordedRequest
 import org.junit.Before
 import org.junit.Test
 import retrofit2.Retrofit
@@ -79,50 +75,4 @@ class ExchangeRatesNetworkRequestTest {
             .baseUrl(baseUrl)
             .build()
     }
-}
-
-class RequestSpecDispatcher(
-    private val requestSpec: RequestSpec,
-    private val responseCreator: (RecordedRequest) -> MockResponse
-) : Dispatcher() {
-
-    override fun dispatch(request: RecordedRequest): MockResponse = with(requestSpec) {
-        if (
-            (headers == null || headers == request.headers)
-            && (method == null || method == request.method)
-            && (headers == null || headers == request.headers)
-        ) {
-            return@with responseCreator(request)
-        } else {
-            throw IllegalArgumentException("Request: $request did not match the given spec: $requestSpec")
-        }
-    }
-}
-
-data class RequestSpec(
-    val path: String? = null,
-    val method: String? = null,
-    val headers: Headers? = null
-)
-
-class RequestSpecBuilder(
-    var path: String? = null,
-    var method: String? = null,
-    var headers: Headers? = null
-)
-
-fun requestSpec(block: RequestSpecBuilder.() -> Unit): RequestSpec =
-    with(RequestSpecBuilder().apply(block)) {
-        RequestSpec(path, method, headers)
-    }
-
-fun forRequest(block: RequestSpecBuilder.() -> Unit): RequestSpec = requestSpec(block)
-
-fun RequestSpec.respondWith(response: MockResponse.(RecordedRequest) -> Unit): RequestSpecDispatcher {
-    val responseCreator = { request: RecordedRequest ->
-        val mockResponse = MockResponse()
-        mockResponse.response(request)
-        mockResponse
-    }
-    return RequestSpecDispatcher(this, responseCreator)
 }
---
+++ exchangerates/src/test/java/com/thedeekay/exchangerates/RequestSpecDispatcher.kt
@@ -0,0 +1,59 @@
+package com.thedeekay.exchangerates
+
+import okhttp3.Headers
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.RecordedRequest
+
+/**
+ * A dispatcher that enables specification of a matching request, along with a response for the
+ * given request.
+ *
+ * Any requests not matching the given specification will throw an exception.
+ */
+class RequestSpecDispatcher(
+    private val requestSpec: RequestSpec,
+    private val responseCreator: (RecordedRequest) -> MockResponse
+) : Dispatcher() {
+
+    override fun dispatch(request: RecordedRequest): MockResponse = with(requestSpec) {
+        if (
+            (headers == null || headers == request.headers)
+            && (method == null || method == request.method)
+            && (headers == null || headers == request.headers)
+        ) {
+            return@with responseCreator(request)
+        } else {
+            throw IllegalArgumentException("Request: $request did not match the given spec: $requestSpec")
+        }
+    }
+}
+
+data class RequestSpec(
+    val path: String? = null,
+    val method: String? = null,
+    val headers: Headers? = null
+)
+
+class RequestSpecBuilder(
+    var path: String? = null,
+    var method: String? = null,
+    var headers: Headers? = null
+)
+
+fun requestSpec(block: RequestSpecBuilder.() -> Unit): RequestSpec =
+    with(RequestSpecBuilder().apply(block)) {
+        RequestSpec(path, method, headers)
+    }
+
+// exists simply to enable 'forRequest {...}.respondWith {...}' syntax
+fun forRequest(block: RequestSpecBuilder.() -> Unit): RequestSpec = requestSpec(block)
+
+fun RequestSpec.respondWith(response: MockResponse.(RecordedRequest) -> Unit): RequestSpecDispatcher {
+    val responseCreator = { request: RecordedRequest ->
+        val mockResponse = MockResponse()
+        mockResponse.response(request)
+        mockResponse
+    }
+    return RequestSpecDispatcher(this, responseCreator)
+}
