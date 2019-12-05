f862891d3e8a54a6b6c04daf64c41b8f05d23ecf
aleksaboskov <aleksa.boskov@lotusflare.com>
2019-12-05 21:49:04 +0100

    Remove unnecessary test rule

--- exchangerates/src/test/java/com/thedeekay/exchangerates/DefaultExchangeRatesRepositoryTest.kt
+++ exchangerates/src/test/java/com/thedeekay/exchangerates/DefaultExchangeRatesRepositoryTest.kt
@@ -4,7 +4,6 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import com.thedeekay.domain.*
-import com.thedeekay.rxtestutils.RxJavaSchedulersRule
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -18,8 +17,6 @@ class DefaultExchangeRatesRepositoryTest {
 
     @get:Rule
     val instantTaskExecutorRule: InstantTaskExecutorRule = InstantTaskExecutorRule()
-    @get:Rule
-    val rxJavaSchedulersRule = RxJavaSchedulersRule()
 
     private lateinit var repository: DefaultExchangeRatesRepository
 
