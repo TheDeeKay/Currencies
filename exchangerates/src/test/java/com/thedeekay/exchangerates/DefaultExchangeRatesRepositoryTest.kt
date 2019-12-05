6b1e462d75822f9c7366dc450a5668a77e1357ca
aleksaboskov <aleksa.boskov@lotusflare.com>
2019-12-05 20:34:57 +0100

    Extract repository creation in tests

--- exchangerates/src/test/java/com/thedeekay/exchangerates/DefaultExchangeRatesRepositoryTest.kt
+++ exchangerates/src/test/java/com/thedeekay/exchangerates/DefaultExchangeRatesRepositoryTest.kt
@@ -1,14 +1,20 @@
 package com.thedeekay.exchangerates
 
 import com.thedeekay.domain.EUR
+import org.junit.Before
 import org.junit.Test
 
 class DefaultExchangeRatesRepositoryTest {
 
+    private lateinit var repository: DefaultExchangeRatesRepository
+
+    @Before
+    fun setUp() {
+        repository = DefaultExchangeRatesRepository()
+    }
+
     @Test
     fun `exchange rates emit an empty list when nothing has been set`() {
-        val repository = DefaultExchangeRatesRepository()
-
         repository.allExchangeRates(EUR).test()
 
             .assertValue(emptyList())
@@ -17,7 +23,6 @@ class DefaultExchangeRatesRepositoryTest {
 
     @Test
     fun `exchange rates are empty when empty list is set`() {
-        val repository = DefaultExchangeRatesRepository()
         repository.setExchangeRates(emptyList(), EUR)
 
         repository.allExchangeRates(EUR).test()
