fcf5fa428fc14a91122e7fbed8f00283c3705c5d
aleksaboskov <aleksa.boskov@lotusflare.com>
2019-12-06 01:21:43 +0100

    Add CalculateRatesUseCase with documentation

---
+++ exchangerates/src/main/java/com/thedeekay/exchangerates/CalculateRatesUseCase.kt
@@ -0,0 +1,19 @@
+package com.thedeekay.exchangerates
+
+import com.thedeekay.domain.Money
+import io.reactivex.Flowable
+
+/**
+ * Continuously calculates what amount of every other currency a given currency amount
+ * could be exchanged for, using latest exchange rates.
+ *
+ * For instance: given 10USD, and rates for USD/GBP and USD/EUR, it will continuously calculate
+ * how much GBP and how much EUR the given 10USD amount to.
+ */
+class CalculateRatesUseCase {
+
+    fun execute(conversionAmount: Money): Flowable<List<Money>> {
+        TODO()
+    }
+
+}
---
+++ exchangerates/src/test/java/com/thedeekay/exchangerates/CalculateRatesUseCaseTest.kt
@@ -0,0 +1,3 @@
+package com.thedeekay.exchangerates
+
+class CalculateRatesUseCaseTest
