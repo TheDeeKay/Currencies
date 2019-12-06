3aca84770d44849496d729c3025daafd1b8a40f9
aleksaboskov <aleksa.boskov@lotusflare.com>
2019-12-06 05:36:17 +0100

    Add test for checking whether currencies are formatted properly

--- app/src/main/java/com/thedeekay/currencies/RatesViewModel.kt
+++ app/src/main/java/com/thedeekay/currencies/RatesViewModel.kt
@@ -1,18 +1,32 @@
 package com.thedeekay.currencies
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.toLiveData
+import com.thedeekay.currencies.CurrencyUiModel.ConvertedCurrency
+import com.thedeekay.currencies.CurrencyUiModel.MainCurrency
+import com.thedeekay.domain.EUR
+import com.thedeekay.domain.Money
 import com.thedeekay.exchangerates.CalculateRatesUseCase
+import java.math.BigDecimal
 
 /**
  * View model for the currencies conversion screen.
  */
-class RatesViewModel(calculateRatesUseCase: CalculateRatesUseCase) : ViewModel() {
+class RatesViewModel(private val calculateRatesUseCase: CalculateRatesUseCase) : ViewModel() {
 
-    val currencyAmounts: LiveData<List<CurrencyUiModel>> = MutableLiveData(
-        listOf(
-            CurrencyUiModel.MainCurrency("EUR", "Euro")
-        )
-    )
+    val currencyAmounts: LiveData<List<CurrencyUiModel>> by lazy {
+        calculateRatesUseCase.execute(Money(BigDecimal("10"), EUR))
+            .map { currencies ->
+                listOf(MainCurrency(EUR.currencyCode, EUR.displayName)) +
+                        currencies.map {
+                            ConvertedCurrency(
+                                it.currency.currencyCode,
+                                it.currency.displayName,
+                                it.amount.toString()
+                            )
+                        }
+            }
+            .toLiveData()
+    }
 }
--- app/src/test/java/com/thedeekay/currencies/RatesViewModelTest.kt
+++ app/src/test/java/com/thedeekay/currencies/RatesViewModelTest.kt
@@ -2,17 +2,15 @@ package com.thedeekay.currencies
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.thedeekay.currencies.CurrencyUiModel.ConvertedCurrency
 import com.thedeekay.currencies.CurrencyUiModel.MainCurrency
-import com.thedeekay.domain.EUR
-import com.thedeekay.domain.Money
-import com.thedeekay.domain.times
+import com.thedeekay.domain.*
 import com.thedeekay.exchangerates.CalculateRatesUseCase
 import com.thedeekay.rxtestutils.RxJavaSchedulersRule
 import io.mockk.every
 import io.mockk.mockk
 import io.reactivex.Flowable
 import org.hamcrest.CoreMatchers.`is`
-import org.junit.After
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Rule
@@ -36,22 +34,43 @@ class RatesViewModelTest {
 
         viewModel = RatesViewModel(calculateRatesUseCase)
 
-        viewModel.currencyAmounts.observeForever(observer)
-    }
-
-    @After
-    fun tearDown() {
-        viewModel.currencyAmounts.removeObserver(observer)
+        observer = Observer { }
     }
 
     @Test
-    fun `initially selected currency is EUR`() {
-        setCalculatedCurrencies(0L * EUR, emptyList())
+    fun `initially selected currency is EUR and is returned when rates are empty`() {
+        setCalculatedCurrencies(10L * EUR, emptyList())
+        viewModel.currencyAmounts.observeForever(observer)
 
         assertThat(
             viewModel.currencyAmounts.value,
             `is`(listOf<CurrencyUiModel>(MainCurrency("EUR", "Euro")))
         )
+        viewModel.currencyAmounts.removeObserver(observer)
+    }
+
+    @Test
+    fun `when rates are present, currencies are properly mapped`() {
+        setCalculatedCurrencies(
+            10L * EUR,
+            listOf(
+                11.1 * USD,
+                9.1 * GBP
+            )
+        )
+        viewModel.currencyAmounts.observeForever(observer)
+
+        assertThat(
+            viewModel.currencyAmounts.value,
+            `is`(
+                listOf(
+                    MainCurrency("EUR", "Euro"),
+                    ConvertedCurrency("USD", "US Dollar", "11.1"),
+                    ConvertedCurrency("GBP", "British Pound Sterling", "9.1")
+                )
+            )
+        )
+        viewModel.currencyAmounts.removeObserver(observer)
     }
 
     private fun setCalculatedCurrencies(
